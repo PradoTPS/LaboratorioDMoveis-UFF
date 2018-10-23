996a5edb857cc5462794e776dcd66d216c8dce97
Lucas <siqueiralpontes@gmail.com>
2018-10-23 20:45:25 -0300

    Criação view de cadastro de evento

---
+++ app/src/main/java/labmoveis/uffeventos/CadastroEvento.java
@@ -0,0 +1,19 @@
+package labmoveis.uffeventos;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+
+public class CadastroEvento extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_cadastro_evento);
+    }
+
+    public void cadastrarEvento(View v){
+        
+
+    }
+}
