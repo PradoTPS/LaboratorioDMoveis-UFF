0616aee04a49d04180067a4ba8069a964aa1cbe5
victate <30375185+victate@users.noreply.github.com>
2018-10-23 21:30:59 -0300

    Adicao de RecycleView para a listagem de eventos e detalhes.

---
+++ app/src/main/java/labmoveis/uffeventos/Events.java
@@ -0,0 +1,36 @@
+package labmoveis.uffeventos;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+
+import java.util.Date;
+
+public class Events extends AppCompatActivity {
+
+    private RecyclerView mRecyclerView;
+    private RecyclerView.Adapter mAdapter;
+    private RecyclerView.LayoutManager mLayoutManager;
+    private Date dt = new Date(198279);
+    private eventItem aux = new eventItem("nome", "local", "descricao","responsavel", "fundo_praia.jpg", dt,5, 100);
+    private eventItem[] myDataset = {aux, aux, aux, aux, aux, aux, aux, aux};
+
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_events);
+        mRecyclerView = (RecyclerView) findViewById(R.id.my_recycler_view);
+
+        // use this setting to improve performance if you know that changes
+        // in content do not change the layout size of the RecyclerView
+        mRecyclerView.setHasFixedSize(true);
+
+        // use a linear layout manager
+        mLayoutManager = new LinearLayoutManager(this);
+        mRecyclerView.setLayoutManager(mLayoutManager);
+
+        // specify an adapter (see also next example)
+        mAdapter = new EventsList(myDataset);
+        mRecyclerView.setAdapter(mAdapter);
+    }
+}
